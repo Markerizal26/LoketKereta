4581328f7a890000a5c6dac5e779ca6541d9f12f
Darriel Markerizal <darrielmarkerizal@gmail.com>
2023-10-22 19:57:49 +0700

    feat(register) : make layout register

---
+++ app/src/main/java/com/example/loketkereta/RegisterActivity.kt
@@ -0,0 +1,17 @@
+package com.example.loketkereta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.loketkereta.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
